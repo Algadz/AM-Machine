cdc65ba3734c9783a0b8d4827fbae41a27885ba4
Alban MOURIER <alban.mourier@gmail.com>
2017-08-30 17:48:12 +0200

    Test file

--- test_rectangle.py
+++ test_rectangle.py
@@ -1,8 +1,10 @@
-from unittest import TestCase
-from MasterGround import Rectangle
+from unittest import TestCase  # import unittest
+from MasterGround import Rectangle  # import Rectangle from MasterGround
 
 
-class TestRectangle(TestCase):
-    def test_Area_Succeed(self):
-        r = Rectangle(2,3)
-        self.assertEquals(r.area(), 6)
+class TestRectangle(TestCase):  # define class TestRectangle
+    def test_Area_Succeed(self):  # define test for area
+        r1 = Rectangle(2, 3)  # i
+        r2 = Rectangle(2, 3.2)  # i
+        self.assertEquals(r1.area(), 6)  # i
+        self.assertEquals(r2.area(), 6.4)  # i
