5aa1a3679e11dbec01c87396d790349446321e0c
Alban MOURIER <alban.mourier@gmail.com>
2017-09-20 13:48:06 +0200

    start of a gcode reader

--- PostProcessor.py
+++ PostProcessor.py
@@ -28,6 +28,7 @@ def get_determinant(v1, v2):  # return the determinant of two vector
 
 
 def get_angle(v1, v2):  # return the trigonometric angle between two vector
+
     if get_determinant(v1, v2) >= 0:
         return math.acos(get_scalar_product(v1, v2) / (get_magnitude(v1) * get_magnitude(v2)))
 
@@ -82,17 +83,17 @@ class Line:
 
             Gcode command must be written uppercase
         """
-        # TODO : ne fait pas la différence si la lettre est dans un commentaire ou non
+        # TODO : implementer G92 G91 G90 M82
         if self.text == "\n":
             self.blank = True
             return
         else:
             self.blank = False
 
-        if len(regex.findall("(?<!;.*)G([0|1])\s", self.text)) > 0:  # if G character exists assign its value, G2 not implemented
-            self.g = regex.findall("(?<!;.*)G([0|1])\s", self.text)[0]
-
-        if self.g is not None:
+        if len(regex.findall("(?<!;.*)G(0|1|3|92|90|91)\s", self.text)) > 0:  # if G character exists assign its value, G2 not implemented
+            self.g = regex.findall("(?<!;.*)G(0|1|3|92|90|91)\s", self.text)[0]
+            print(self.text," -- g", self.g)
+        if self.g in ["0", "1", "92"]:
             if len(regex.findall("(?<!;.*)X(\+?-?\d*\.?\d*)", self.text)) > 0:  # assign value to x (pos, neg, int, float accepted)
                 self.x = float(regex.findall("(?<!;.*)X(\+?-?\d*\.?\d*)", self.text)[0])
 
@@ -103,6 +104,9 @@ class Line:
 
             if len(regex.findall("(?<!;.*)Z(\+?-?\d*\.?\d*)", self.text)) > 0:  # assign value to z (pos, neg, int, float accepted)
                 self.z = float(regex.findall("(?<!;.*)Z(\+?-?\d*\.?\d*)", self.text)[0])
+
+            if len(regex.findall("(?<!;.*)U(\+?-?\d*\.?\d*)", self.text)) > 0:  # assign value to z (pos, neg, int, float accepted)
+                self.ct = float(regex.findall("(?<!;.*)U(\+?-?\d*\.?\d*)", self.text)[0])
 
         if len(regex.findall("(?<!;.*)E(\+?-?\d*\.?\d*)", self.text)) > 0:      # assign value to e (pos, neg, int, float accepted)
             self.e = float(regex.findall("(?<!;.*)E(\+?-?\d*\.?\d*)", self.text)[0])
@@ -117,6 +121,8 @@ class Line:
         if len(regex.findall(";(.*)", self.text)) > 0:                  # Comments must be at the end of line or solo
             self.comment = regex.findall(";(.*)\\n", self.text)[0]
 
+# todo : prendre en compte les deplacement nuls  /0 ds magnitude
+# todo : prendre en compte les commandes G sans X ou Y
 
 class Gcode:
     def __init__(self, address):
@@ -173,11 +179,25 @@ class Gcode:
                 counter += 1
             self.get_g_command_ref()
 
+    def transform_xy(self):
+
+        self.get_g_command_ref()
+        counter = 0
+
+        while counter < len(self.gLineRefList):
+            actual_point = self.lineList[self.gLineRefList[counter]]
+            transformed_point = get_vector([0, 0], rotate_point(actual_point.point, actual_point.ct*math.pi/180))
+
+            actual_point.xt = round(transformed_point[0], 3)
+            actual_point.yt = round(transformed_point[1], 3)
+
+            counter += 1
+
     def get_g_command_ref(self):
         counter = 0
-        self.gLineRefList=[]
+        self.gLineRefList = []
         for line in self.lineList:   # Get list of line reference containing a G movement
-            if line.g is not None:
+            if line.g in ["1", "0"]:
                 self.gLineRefList.append(counter)
                 counter += 1
             else:
@@ -185,7 +205,8 @@ class Gcode:
 
     def get_c_axis(self):
         """
-        Method to calculate the value of the C axis for each G0/G1 command
+            Method to calculate the value of the C axis for each G0/G1 command
+            each point must have the orientation of the vector to come
         """
 
         counter = 0
@@ -194,59 +215,78 @@ class Gcode:
             start_point = self.lineList[self.gLineRefList[counter]]
             end_point = self.lineList[self.gLineRefList[counter + 1]]
 
-            end_point.ct = get_angle(get_vector(start_point.point, end_point.point), toolVector) * 180 / math.pi  # calculation of ct
-            end_point.ct = round(end_point.ct, 3)
+            if start_point.x == end_point.x and start_point.y == end_point.y:   # todo : Dégueulasse , problème lorsque point double
+                start_point.point[0] += 0.001
+
+
+            start_point.ct = get_angle(get_vector(start_point.point, end_point.point), toolVector) * 180 / math.pi  # calculation of ct
+            start_point.ct = round(start_point.ct, 3)
+
             counter += 1
 
+        if counter == len(self.gLineRefList) - 1:
+            self.lineList[self.gLineRefList[counter]].ct = self.lineList[self.gLineRefList[counter-1]].ct
         # the following line apply c axis for G0 command
-
-    def correct_c_for_g0(self):
-        """
-        Method to correct the values of the C axis for G0 commands
-        Each G0 command has the C axis value of the next G1 command
-        """
-
-        counter = len(self.gLineRefList) - 1
-        variable = None
-
-        while counter != -1:
-            if self.lineList[self.gLineRefList[counter]].g is "1":
-                variable = self.lineList[self.gLineRefList[counter]].ct
-            if self.lineList[self.gLineRefList[counter]].g is "0":
-                self.lineList[self.gLineRefList[counter]].ct = variable
-            counter -= 1
 
     def clean_c_axis(self):
 
-        counter = 1         # no angle for first element
-
+        counter = 0         # no angle for first element
+        self.get_g_command_ref()
         while counter < len(self.gLineRefList)-1:     # -1 because len([1,2,3])=3
-
             angle1 = self.lineList[self.gLineRefList[counter]].ct
             angle2 = self.lineList[self.gLineRefList[counter+1]].ct
             angle2 = angle2 - round((angle2 - angle1)/360)*360
             self.lineList[self.gLineRefList[counter+1]].ct = angle2
             counter += 1
 
+        if counter == len(self.gLineRefList) - 1:
+            self.lineList[self.gLineRefList[counter]].ct = self.lineList[self.gLineRefList[counter - 1]].ct
+
+    def transform_pos(self, line, angle):
+        transformed_pos = get_vector([0, 0], rotate_point(line.point, angle * math.pi / 180))
+
+        line.xt = round(transformed_pos[0], 3)
+        line.yt = round(transformed_pos[1], 3)
+
     def transform_xy(self):
-        """
-        To be executed after C calculation
-        Method returning values for xt and yt to correct rotation
 
-        Strategy :
-        parcourir la liste des points g
-        retrancher au point le vecteur entre la position modifiée et la position d'origine
-
-        """
         self.get_g_command_ref()
-        counter = 0
 
-        while counter < len(self.gLineRefList)-1:
+        counter = 0
+        while counter < len(self.gLineRefList):
+
             actual_point = self.lineList[self.gLineRefList[counter]]
-            transformed_point = get_vector(rotate_point(actual_point.point, actual_point.ct*math.pi/180), [0, 0])
-            actual_point.xt = -round(transformed_point[0], 3)
-            actual_point.yt = -round(transformed_point[1], 3)
+            self.transform_pos(actual_point, actual_point.ct)
+
             counter += 1
+
+    def improve_trajectory(self, max_angle):
+
+    # pour chaque g1 point append ce meme point transformé par c-1
+        self.get_g_command_ref()
+
+        counter = len(self.gLineRefList)-1
+        while counter != 1:
+            if self.lineList[self.gLineRefList[counter]].g == "1":
+
+                actual_point = self.lineList[self.gLineRefList[counter]]
+
+                prev_point = self.lineList[self.gLineRefList[counter-1]]
+                if abs(actual_point.ct-prev_point.ct) > max_angle:
+                    actual_point.g = "0"
+                    actual_point.e = None
+                    new_point = Line(actual_point.text)
+                    self.transform_pos(new_point, prev_point.ct)
+                    new_point.ct = prev_point.ct
+                    new_point.g = "1"
+                    self.lineList.insert(self.gLineRefList[counter], new_point)
+
+                    counter -= 1
+                else:
+                    counter -=1
+
+            else:
+                counter -= 1
 
     def print_gcode(self):
         for line in self.lineList:
@@ -260,12 +300,19 @@ class Gcode:
             if line.z is not None:
                 text += "Z%s " % line.z
             if line.ct is not None:
-                text += "C%s " % (line.ct*-1)
+                text += "U%s " % (line.ct*-1)
             if line.e is not None:
                 text += "E%s " % line.e
+            if line.m is not None:
+                if line.m[0] is not "":
+                    text += "M%s " % line.m[0]
+                if line.m[1] is not "":
+                    text += "S%s " % line.m[1]
             if line.comment is not None:
                 text += ";%s" % line.comment
-            print(text)
+
+
+            f.write(text + '\n')
 
     def graph_gcode(self):
         self.get_g_command_ref()
@@ -293,15 +340,17 @@ class Gcode:
 
 gcode = Gcode("test.gcode")
 gcode.get_g_command_ref()
-gcode.graph_gcode()
-#gcode.interpolate(5)
 
 gcode.get_c_axis()
 gcode.clean_c_axis()
-gcode.correct_c_for_g0()
 gcode.transform_xy()
+gcode.improve_trajectory(25)
+
+open('4axis.gcode', 'w').close()
+f = open('4axis.gcode', 'a')
 gcode.print_gcode()
-gcode.graph_gcode()
+f.close()
+
 sys.exit("End of test")
 
 
--- test_rectangle.py
+++ test_rectangle.py
@@ -1,4 +1,3 @@
 import regex
-txt = "E125 ;    E3  E69"
-
-print(regex.findall("(?<!;.*)E(\d*)", txt))
+text = "G92 G3 X0 E0"
+print(regex.findall("(?<!;.*)G(0|1|3|92|90|91)\s", text)) # todo : include 2 char
